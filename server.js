2ca1bddffd4e3c1f364443ca8fead2697224cdcd
Styles <bcs_2021035@iiitm.ac.in>
2023-05-30 23:16:36 +0530

    added packages

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 import express from "express"
 import * as dotenv from 'dotenv' 
 dotenv.config()
+import cors from "cors"
+import mongoose from "mongoose"
 
 const app = express();
 const PORT = process.env.PORT
 app.use(express.json());
+app.use(cors())
+
+mongoose.connect(process.env.MONG_URI);
 
 app.listen(PORT, ()=>{
     console.log(`Listening on http://localhost:${PORT}/`)
