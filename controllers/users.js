1b3311ae02fadaec65b461b9eed51244184952a0
Styles <bcs_2021035@iiitm.ac.in>
2023-06-17 15:32:29 +0530

    added getUser function

--- controllers/users.js
+++ controllers/users.js
@@ -12,3 +12,11 @@ export const createUser = (req, res) => {
       res.send(err);
     });
 };
+
+export const getUser = (req, res) => {
+  const email = req.params['id'] + '@iiitm.ac.in'
+
+  users.findOne({email})
+    .then((obj) => {res.send(obj)})
+    .catch(err => res.send(err))
+}
--- routes/routes.js
+++ routes/routes.js
@@ -1,9 +1,12 @@
 import { Router } from "express";
-import { createUser } from "../controllers/users.js";
+import { createUser, getUser } from "../controllers/users.js";
 
 const router = Router();
 
 // Create New User
 router.post("/register", createUser);
+
+// Get User Details
+router.get("/user/:id", getUser);
 
 export default router;
