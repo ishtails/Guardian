d6e0868d16aae1af743cf3341dfcae15c2c9d5e2
Styles <bcs_2021035@iiitm.ac.in>
2023-06-20 19:14:22 +0530

    added authenticate middleware for authorization

--- routes/api.js
+++ routes/api.js
@@ -4,18 +4,21 @@ import {
   getUser,
   getStudents,
   updateUser,
+  loginUser,
 } from "../controllers/common.js";
 import { openGateEntry } from "../controllers/students.js";
 import { openEntries } from "../controllers/security.js";
+import { authenticate } from "../middlewares/auth.js";
 
 const router = Router();
 
 // POST
 router.post("/register", registerUser);
+router.post("/login", loginUser);
 router.post("/student/:username/exit-request", openGateEntry);
 
 // GET
-router.get("/users/:username", getUser);
+router.get("/users/:username", authenticate, getUser);
 router.get("/students", getStudents);
 router.get("/students/open", openEntries);
 
