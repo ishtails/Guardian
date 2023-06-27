d2ab7118056c34c29379e7068717c0155abd6ccd
Styles <bcs_2021035@iiitm.ac.in>
2023-06-27 21:20:35 +0530

    verifyOTP / isRegistered

--- middlewares/middlewares.js
+++ middlewares/middlewares.js
@@ -6,11 +6,12 @@ import fs from "fs";
 import path from "path";
 import { fileURLToPath } from "url";
 import otpGenerator from "otp-generator";
+import users from "../models/userModel.js";
 
 // Check Session
 export const requireAuth = (req, res, next) => {
   if (!req.session.username) {
-    return res.status(401).json({ error: "Unauthorized" });
+    return res.status(401).json({ error: "Not Logged In" });
   }
 
   next();
@@ -43,29 +44,21 @@ export const verifyOutingChecks = (req, res, next) => {
 };
 
 // OTP Generation
-export const sendOTP = async (req, res, next) => {
+export const sendOTP = async (req, res) => {
   try {
-    //Institute Email Validation
-    const emailSchema = Joi.object({
-      email: Joi.string()
-        .email()
-        .required()
-        .custom((value, helpers) => {
-          if (value.endsWith("@iiitm.ac.in")) {
-            return value;
-          } else {
-            return helpers.error("any.invalid");
-          }
-        }, "Custom Domain Validation"),
-    });
-    await emailSchema.validateAsync(req.body);
-
-    //Generate OTP
+    //Generate & Store OTP in redis
     const otp = otpGenerator.generate(6, {
       upperCaseAlphabets: false,
       lowerCaseAlphabets: false,
       specialChars: false,
     });
+
+    if (req.body.email) {
+      req.session.email = req.body.email;
+    }
+
+    req.session.otp = otp;
+    req.session.otpExpiry = Date.now() + 300000; //5 Minutes from now
 
     //load OTP Email template
     const __dirname = path.dirname(fileURLToPath(import.meta.url));
@@ -82,16 +75,40 @@ export const sendOTP = async (req, res, next) => {
     };
 
     const result = await sendMail(mailOptions);
-    res.send(result);
-
+    return res.json({ message: "OTP Sent Successfully!", result });
   } catch (error) {
     res.status(500).json({ message: "ERROR: " + error });
   }
 };
 
 // OTP Verification
-export const verifyOTP = (req, res, next) => {
-  next();
+export const verifyOTP = async (req, res) => {
+  try {
+    const userEnteredOTP = req.body.otp;
+    const storedOTP = req.session.otp;
+    const storedOTPExpiry = req.session.otpExpiry;
+
+    if (!storedOTP || !storedOTPExpiry) {
+      return res.status(401).send("No OTP generated!");
+    }
+
+    if (Date.now() > storedOTPExpiry) {
+      delete req.session.otp;
+      delete req.session.otpExpiry;
+      return res.status(401).send("OTP expired!");
+    }
+
+    if (userEnteredOTP === storedOTP) {
+      delete req.session.otp;
+      delete req.session.otpExpiry;
+      req.session.tempSessionExp = Date.now() + 300000; //5 Minutes from now
+      res.send("OTP Verified Successfully!");
+    } else {
+      return res.status(400).send("Wrong OTP!");
+    }
+  } catch (error) {
+    return res.status(500).json("ERROR: " + error);
+  }
 };
 
 // Send Email
@@ -109,5 +126,41 @@ export const sendMail = async (mailOptions) => {
     return info;
   } catch (error) {
     return { message: "ERROR: " + error };
+  }
+};
+
+export const isRegistered = async (req, res, next) => {
+  try {
+    const email = req.body.email;
+
+    //Email Validation
+    const emailSchema = Joi.object({
+      email: Joi.string()
+        .email()
+        .required()
+        .custom((value, helpers) => {
+          if (value.endsWith("@iiitm.ac.in")) {
+            return value;
+          } else {
+            return helpers.error("any.invalid");
+          }
+        }, "Custom Domain Validation"),
+    });
+
+    await emailSchema.validateAsync({ email });
+
+    // isRegistered
+    const user = await users.findOne({ email }, { _id: 0, username: 1 });
+    if (user) {
+      res.status(200).send({  email, isRegistered: true });
+    } else {
+      res.status(200).send({  email, isRegistered: false });
+    }
+  } catch (error) {
+    if(error.details){
+      return res.status(422).json(error.details)
+    }
+
+    return res.status(500).json("ERROR: " + error);
   }
 };
