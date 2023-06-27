d305953f5b5167442ab6a99980b47e06bf6f0604
Aneeka Mangal <aneekamangal@gmail.com>
2023-06-27 18:17:11 +0530

    setup cloudinary

--- server.js
+++ server.js
@@ -58,6 +58,13 @@ mongoose
     console.log(err);
   });
 
+// Configure Cloudinary
+cloudinary.config({
+  cloud_name: 'your_cloud_name',
+  api_key: 'your_api_key',
+  api_secret: 'your_api_secret',
+});
+
 // Root Route
 app.get("/", (req, res) => {
   res.send("Server Home Page");
