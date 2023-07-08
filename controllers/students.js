a054aefcb414c772763563169afb11ff7777871e
Styles <bcs_2021035@iiitm.ac.in>
2023-07-08 05:51:38 +0530

    Update students.js

--- controllers/students.js
+++ controllers/students.js
@@ -1,22 +1,29 @@
 import outings from "../models/outingModel.js";
+import users from "../models/userModel.js";
 
 //Open Outing Entry
 export const openGateEntry = async (req, res) => {
   try {
-    if(req.session.role !== "student"){
-      return res.json("Only for students")
+    if (req.session.role !== "student") {
+      return res.status(403).json("Only for students");
     }
 
     const { username } = req.session;
     const { reason } = req.body;
 
-    const result = await outings.findOne({ username, isOpen: true });
+    const result = await outings.findOne(
+      { username, isOpen: true },
+      { username: 1 }
+    );
     if (result) {
       return res.status(400).json("Already outside");
     }
 
+    const { gender } = await users.findOne({ username }, { gender: 1 });
+
     const newOuting = new outings({
       username,
+      gender,
       reason,
     });
 
@@ -31,18 +38,18 @@ export const openGateEntry = async (req, res) => {
 export const isOutside = async (req, res) => {
   try {
     const { username } = req.session;
-    const result = await outings.findOne({ username, isOpen: true }, {reason:1, outTime:1});
+    const result = await outings.findOne(
+      { username, isOpen: true },
+      { reason: 1, outTime: 1 }
+    );
 
     if (!result) {
       return res.status(200).json("inside");
     } else {
       const { reason, outTime } = result;
-      return res
-        .status(200)
-        .json("outside");
+      return res.status(200).json("outside");
     }
   } catch (error) {
     return res.status(500).send(error.message);
   }
 };
-
